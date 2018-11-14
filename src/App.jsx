6e71039ff212e67d3a41d44844587938826d4e85
Anatoliy Zaslavskiy <toli@tolicodes.com>
2018-11-13 20:55:50 -0800

    Scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+import Map from './components/Map';
+import Sidebar from './components/Sidebar';
+
+export default () => (
+  <div>
+    <Header />
+    <Map />
+    <Sidebar />
+  </div>
+);
