f39548cc24f36c8a2b99cd152e1fe9665bac69f3
Anatoliy Zaslavskiy <toli@tolicodes.com>
2018-11-14 08:47:23 -0800

    more experimentation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Map, TileLayer } from 'react-leaflet';
 
 export default () => (
   <div>
-    Yo
+    <Map
+      center={{
+        lat: 51.505,
+        lng: -0.09,
+      }}
+    >
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </Map>
   </div>
 );
